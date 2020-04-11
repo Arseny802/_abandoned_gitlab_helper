c61a3642b26ed058b1a960b3c0641800cec96658
Arseny802 <Arseny802@yandex.ru>
2020-04-12 01:46:35 +0300

    logging application start

--- bot.py
+++ bot.py
@@ -1,13 +1,19 @@
 from bot.controller import Controller
+from common.logger import LoggerInstance
 
 
-def main():
+def bot_controller_process():
+    logger = LoggerInstance().logger
+    logger.info("Application started.")
     controller = Controller()
+    cycle_iteration: int = 1
     # FIXME: exception catcher at process level
     while True:
+        logger.info("Started new work process number {}.".format(cycle_iteration))
         controller.process()
         controller.sleep()
+        cycle_iteration += 1
 
 
 if __name__ == "__main__":
-    main()
+    bot_controller_process()
